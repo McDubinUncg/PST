3e8169836d1f79e0745c88e2aef859668fab2267
John Abdellmelk <jnabdell@uncg.edu>
2023-04-14 15:51:40 -0400

    test

---
+++ src/main/java/com/csc340sp23/controller/HomeController.java
@@ -0,0 +1,44 @@
+package com.csc340sp23.controller;
+
+import com.csc340sp23.domain.Hyperlink;
+import com.csc340sp23.repository.HyperlinkRepository;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private HyperlinkRepository hyperlinkRepository;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        System.out.println("Home method called!");
+        List<Hyperlink> hyperlinks = hyperlinkRepository.findAll();
+        model.addAttribute("hyperlinks", hyperlinks);
+        return "home";
+    }
+
+    @GetMapping("/threads")
+    public String threads(Model model) {
+        System.out.println("Threads method called!");
+        return "threads";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        System.out.println("Login method called!");
+        return "login";
+    }
+
+    @GetMapping("/favorites")
+    public String favorites(Model model) {
+        System.out.println("Favorites method called!");
+        return "favorites";
+    }
+}
