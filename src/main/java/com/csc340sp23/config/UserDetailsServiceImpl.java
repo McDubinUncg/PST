9d2012f04d4b1c64ae1316571f90c74c37ee6f75
John Abdellmelk <jnabdell@uncg.edu>
2023-04-17 14:35:18 -0400

    tried working on login but ran into Servlet error

---
+++ src/main/java/com/csc340sp23/config/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.csc340sp23.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+/**
+ *
+ * @author those
+ */
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = userRepository.getUserByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Could not find user");
+        }
+
+        return new MyUserDetails(user);
+    }
+}
---
+++ src/main/java/com/csc340sp23/config/UserRepository.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.csc340sp23.config;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+/**
+ *
+ * @author those
+ */
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    public User getUserByUsername(@Param("username") String username);
+}
